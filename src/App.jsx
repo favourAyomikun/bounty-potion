605d20b67097f9acee9c4acc5742d5c8f3cfe923
Oshoaro Favour <oshoarofavour@gmail.com>
2025-02-15 16:55:42 +0100

    installed tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-xl text-yellow-500 animate-bounce'>Habib is my friend?...... Noo👍</div>
+    </>
+  )
+}
+
+export default App
